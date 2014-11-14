eeca4c15161d5d05d0d5b817b9fc5d935c5f7fa1
Michael Tang <mike_t1992@hotmail.com>
2014-11-13 20:22:27 -0800

    findbugparser auto

--- src/findbugparser/FindBugMainJ.java
+++ src/findbugparser/FindBugMainJ.java
@@ -1,16 +1,20 @@
+import java.io.IOException;
 import java.util.Map;
 
 
 public class FindBugMainJ {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Map<String, Integer> findBugHashMap;
 			
-		  //String test = "StockWatcher"
-		  //def scriptPath = "/Users/me/foo/example.pl"
-		 String command = "java -jar findbugs-3.0.0/lib/findbugs.jar -textui -xml -output OUTPUT.xml " + args[0];
-		 System.out.println(command);
+		//String test = "StockWatcher"
+		//def scriptPath = "/Users/me/foo/example.pl"
+		Runtime rt = Runtime.getRuntime();
+		String command = "java -jar findbugs-3.0.0/lib/findbugs.jar -textui -xml -output OUTPUT.xml " + args[0];
+		Process pr = rt.exec(command);
+		pr.waitFor();
+		//System.out.println(command);
 		  String findBugsXML = "OUTPUT.xml";
-		  System.out.println(findBugsXML);
+		  //System.out.println(findBugsXML);
 		  try{
 			  findBugHashMap = FindBugParser.findBugParser(findBugsXML);
 			  System.out.println(findBugHashMap);
