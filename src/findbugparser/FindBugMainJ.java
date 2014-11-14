0aface80d8b4330cd3ae53e99699539a12ce36cb
Michael Tang <mike_t1992@hotmail.com>
2014-11-13 20:09:20 -0800

    findbugparser auto

---
+++ src/findbugparser/FindBugMainJ.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+
+
+public class FindBugMainJ {
+	public static void main(String[] args) {
+		Map<String, Integer> findBugHashMap;
+			
+		  //String test = "StockWatcher"
+		  //def scriptPath = "/Users/me/foo/example.pl"
+		 String command = "java -jar findbugs-3.0.0/lib/findbugs.jar -textui -xml -output OUTPUT.xml " + args[0];
+		 System.out.println(command);
+		  String findBugsXML = "OUTPUT.xml";
+		  System.out.println(findBugsXML);
+		  try{
+			  findBugHashMap = FindBugParser.findBugParser(findBugsXML);
+			  System.out.println(findBugHashMap);
+		  } catch (Exception e) {
+		}
+		  //println args[0]
+		   
+		   //java -jar findbugs-3.0.0/lib/findbugs.jar
+	   
+		}
+}
