43a29dea8c94299caf44028f82c68c358ed0f9b7
Johnny Jiang <whotakesmyid@gmail.com>
2014-11-17 22:07:33 -0800

    Hashmap of parser completed

--- DepParser/src/depParser.java
+++ DepParser/src/depParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -20,35 +22,47 @@ public class depParser {
 		  System.out.println("Root element " + doc.getDocumentElement().getNodeName());
 		  NodeList nodeLst = doc.getElementsByTagName("class");
 		  System.out.println("Information of all classes");
-	
+		  String temp="";
+
 		  for (int s = 0; s < nodeLst.getLength(); s++) {
-	
+			
 		    Node fstNode = nodeLst.item(s);
 		    //System.out.println("test" + s);
+		    //if( ((Element) fstNode).getAttributeNode("confirmed").equals("confirmed=yes")){
+		    //System.out.println(((Element) fstNode).getAttribute("confirmed"));
 		    
 		    if (fstNode.getNodeType() == Node.ELEMENT_NODE) {
 		      //System.out.println("test" + s + ".1");
 		      Element fstElmnt = (Element) fstNode;
-		      NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("name");
-		      
-		      Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
-		      NodeList fstNm = fstNmElmnt.getChildNodes();
-		      
-		      System.out.println("Class: "  + ((Node) fstNm.item(0)).getNodeValue());
-		      
-		      
-		      NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("inbound");
-		      int NumDep = 0;
-		      for(int i=0; i<lstNmElmntLst.getLength(); i++){
-			      Element lstNmElmnt = (Element) lstNmElmntLst.item(i);
-			      NodeList lstNm = lstNmElmnt.getChildNodes();
-			      //Uncomment the following line of code to find out the exact dependency
-			      //System.out.println("		Inbound dependency: " + ((Node) lstNm.item(0)).getNodeValue());
-			      NumDep = i;
-			  }
-		      System.out.println("	Number of dependency:" + NumDep);
+		      if(fstElmnt.getAttribute("confirmed").equals("yes")){
+			      NodeList fstNmElmntLst = fstElmnt.getElementsByTagName("name");
+			      Element fstNmElmnt = (Element) fstNmElmntLst.item(0);
+			      NodeList fstNm = fstNmElmnt.getChildNodes();
+			      
+			      temp = ((Node) fstNm.item(0)).getNodeValue();
+			      
+			      System.out.println("Class: "  + temp.substring(temp.lastIndexOf('.')+1).trim());
+			      System.out.println("	"+fstElmnt.getAttributeNode("confirmed"));			      
+			      
+			      NodeList lstNmElmntLst = fstElmnt.getElementsByTagName("outbound");
+			      
+			      int NumDep = 0;
+			      for(int i=0; i<lstNmElmntLst.getLength(); i++){
+				      Element lstNmElmnt = (Element) lstNmElmntLst.item(i);
+				      if(lstNmElmnt.getAttribute("type").equals("class")){
+				    	  if(lstNmElmnt.getAttribute("confirmed").equals("yes")){
+				    		  NodeList lstNm = lstNmElmnt.getChildNodes();
+						      //Uncomment the following line of code to find out the exact dependency
+				    		  temp = ((Node) lstNm.item(0)).getNodeValue();
+						      System.out.println("	Out dependency: " + temp.substring(temp.lastIndexOf('.')+1).trim());
+						      NumDep = i;
+				    	  }
+				      }
+				  }
+			      System.out.println("	Number of dependency:" + NumDep);
 	    }
 
+	  }
 	  }
 	  } catch (Exception e) {
 	    e.printStackTrace();
