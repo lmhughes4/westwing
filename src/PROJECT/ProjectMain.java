5a5fac105238b6c03f94a175f24bfbeec3f83eb8
Michael Tang <mike_t1992@hotmail.com>
2014-11-20 18:53:26 -0800

    updated projectmain

--- src/PROJECT/ProjectMain.java
+++ src/PROJECT/ProjectMain.java
@@ -1,18 +1,15 @@
-package PROJECT;
-
 import java.io.IOException;
 import java.util.Map;
 
-import lineCounter.LineCounter;
-
-import findbugparser.FindBugParser;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class ProjectMain {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		Map<String, Integer> findBugHashMap;
-		Map<String, Integer> locHashMap;
-		Map<String, Map<String,Integer>> depHashMap;
+		Map<String, Integer> findBugHashMap = null;
+		Map<String, Integer> locHashMap = null;
+		Map<String, Map<String,Integer>> depHashMap = null;
 			
 		//String test = "StockWatcher"
 		//def scriptPath = "/Users/me/foo/example.pl"
@@ -20,7 +17,9 @@ public class ProjectMain {
 		String command = "java -jar findbugs-3.0.0/lib/findbugs.jar -textui -xml -output fbOutput.xml " + args[0];
 		Process pr = rt.exec(command);
 		pr.waitFor();
-		command = "cmd /C DependencyExtractor -xml -out depOutput.xml " + args[0];
+		//command = "cmd /C DependencyExtractor -xml -out depOutput.xml " + args[0];
+		command = "DependencyExtractor -xml -out depOutput.xml " + args[0];
+
 		pr = rt.exec(command);
 		pr.waitFor();
 		//System.out.println(command);
@@ -28,18 +27,27 @@ public class ProjectMain {
 		//System.out.println(findBugsXML);
 		try{
 			findBugHashMap = FindBugParser.findBugParser(findBugsXML);
-			System.out.println(findBugHashMap);
-			System.out.println("/n");
+			//System.out.println(findBugHashMap);
+			//System.out.println("/n");
 		  } catch (Exception e) {
 		}
 		String depFinder = "depOutput.xml";
 		try{
 			depHashMap = depParser.depFinderParser(depFinder);
-			System.out.println(depHashMap);
-			System.out.println("/n");
+			//System.out.println(depHashMap);
+			//System.out.println("/n");
 		} catch (Exception e) {
 		}
 		locHashMap = LineCounter.locParser(args[0]); 
-		System.out.println(locHashMap);
+		//System.out.println(locHashMap);
+		
+		try {
+			JSONObject jsonInput = Fuser.mapToJson(Fuser.fuse(findBugHashMap,depHashMap,locHashMap));
+			System.out.println("json: " + jsonInput);
+
+		} catch (JSONException e) {
 		}
+
+		}
+
 }
